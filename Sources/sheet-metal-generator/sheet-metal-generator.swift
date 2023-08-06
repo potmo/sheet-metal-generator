1d49ca61448b3fbb98a1eb952e85a0255d7a5ab8
Nisse Bergman <git@potmo.com>
2023-08-06 23:40:19 +0200

    feat: use orthographic perspective transforms

--- Sources/sheet-metal-generator/sheet-metal-generator.swift
+++ Sources/sheet-metal-generator/sheet-metal-generator.swift
@@ -6,7 +6,7 @@ typealias Vector = simd_double3
 typealias Quat = simd_quatd
 
 @main
-struct HelloWorld: App {
+struct SheetMetalGenerator: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     @StateObject var state = InputState()
@@ -41,26 +41,62 @@ struct HelloWorld: App {
                 }
 
                 HStack(spacing: 0) {
-                    DoubleSlider(label: "Orbit", value: $state.cameraOrbit, range: 0.0 ... 360.0)
+                    DoubleSlider(label: "Orbit", value: $state.cameraOrbit, range: -90.0 ... 90.0)
                     DoubleSlider(label: "Tilt", value: $state.cameraTilt, range: -90.0 ... 90.0)
                     DoubleSlider(label: "Side", value: $state.cameraDollySide, range: -100.0 ... 100.0)
                     DoubleSlider(label: "Up", value: $state.cameraDollyUp, range: -100.0 ... 100.0)
                 }
             }
 
-            VStack {
-                HStack {
+            VStack(spacing: 0) {
+                HStack(spacing: 0) {
                     // CanvasView(state: state, maker: FlatView())
-                    CanvasView(state: state, maker: FlatSideView(), renderTransform: AxisAlignedOrthographicTransform(plane: .xy))
+                    // CanvasView(state: state, maker: FlatSideView(), renderTransform: AxisAlignedOrthographicTransform(plane: .xy))
+
+                    // Top view
                     CanvasView(state: state, maker: FlatSideView(),
-                               renderTransform: PerspectiveTransform(camera: StateObjectCamera(state: state)))
+                               renderTransform: OrthographicTransform(camera: StaticCamera(position: Vector(0, 0, 200),
+                                                                                           rotation: Quat(angle: 0,
+                                                                                                          axis: Vector(0, 0, 1)))))
+
+                    let cameraOrbit = Quat(angle: 0, axis: Vector(0, 0, 1))
+                    let cameraTilt = Quat(angle: .pi / 2, axis: cameraOrbit.act(Vector(1, 0, 0)))
+
+                    // Front view
+                    CanvasView(state: state, maker: FlatSideView(),
+                               renderTransform: OrthographicTransform(camera: StaticCamera(position: Vector(0, 0, 200),
+                                                                                           rotation: cameraTilt * cameraOrbit)))
                 }
-                HStack {
-                    CanvasView(state: state, maker: FlatSideView(), renderTransform: AxisAlignedOrthographicTransform(plane: .xz))
-                    CanvasView(state: state, maker: FlatSideView(), renderTransform: AxisAlignedOrthographicTransform(plane: .yz))
+                HStack(spacing: 0) {
+                    // left view
+                    let cameraOrbit = Quat(angle: -.pi / 2, axis: Vector(0, 0, 1))
+                    let cameraTilt = Quat(angle: .pi / 2, axis: cameraOrbit.act(Vector(1, 0, 0)))
+
+                    // Front view
+                    CanvasView(state: state, maker: FlatSideView(),
+                               renderTransform: OrthographicTransform(camera: StaticCamera(position: Vector(0, 0, 200),
+                                                                                           rotation: cameraTilt * cameraOrbit)))
+
+                    CanvasView(state: state, maker: FlatSideView(),
+                               renderTransform: OrthographicTransform(camera: StateObjectCamera(state: state)))
                 }
             }
         }
+    }
+}
+
+private struct StaticCamera: PerspectiveCamera {
+    let position: Vector
+    let rotation: Quat
+
+    init(position: Vector, rotation: Quat) {
+        self.position = position
+        self.rotation = rotation
+    }
+
+    init(position: Vector, lookDirection: Vector) {
+        self.position = position
+        self.rotation = Quat(from: Vector(0, 0, -1), to: lookDirection.normalized)
     }
 }
 
@@ -71,13 +107,27 @@ private struct StateObjectCamera: PerspectiveCamera {
     }
 
     var position: Vector {
-        return rotation.act(Vector(state.cameraDollySide, -120, state.cameraDollyUp))
+        /*
+         let matrix = matrix_identity_double4x4 *
+             simd_double4x4(translate: simd_double3(x: state.cameraDollySide, y: -200, z: state.cameraDollyUp)) *
+             simd_double4x4(pitch: state.cameraTilt.degreesToRadians, jaw: 0, roll: state.cameraOrbit.degreesToRadians)
+
+         let lastColumn = matrix.columns.3
+
+         */
+        // let computed_transform = matrix_identity_float4x4 * combinedRotationTransform.matrix * translationTransform.matrix
+
+        // return rotation.act(Vector(state.cameraDollySide, -200, state.cameraDollyUp))
+
+        return Vector(0, 0, 200)
     }
 
     var rotation: Quat {
         let cameraOrbit = Quat(angle: state.cameraOrbit.degreesToRadians, axis: Vector(0, 0, 1))
         let cameraTilt = Quat(angle: -state.cameraTilt.degreesToRadians, axis: cameraOrbit.act(Vector(1, 0, 0)))
-        return cameraOrbit
+        return cameraTilt * cameraOrbit
+
+        //  return Quat(pitch: state.cameraTilt.degreesToRadians, jaw: 0, roll: state.cameraOrbit.degreesToRadians)
     }
 }
 
